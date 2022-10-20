5c684b82c9d151758be45e7ea2f97b6e7b9c1eb4
Reggie P <reginaldpitts1@gmail.com>
2022-10-20 15:33:32 -0500

    Updated readme, edited js file to remove inLength, added screenshot

--- README.md
+++ README.md
@@ -1,9 +1,26 @@
 # PasswordGenerator-3
 
+I created a password generator with JavaScript. CSS, and HTML. The user is able to create a random password with special characters, numbers, upper case and lower case letters. The length has to be from 10 to 130 characters long. I was able to use several different variables, functions and for loops to create this. 
 
-I created a password generator with JavaScript. The user is able to create a random password with special characters, numbers, upper case and lower case letters. The length has to be from 8 to 128 characters long. I was able to use several different variables, functions and for loops to create this. 
+# live site: 
 
-live site: 
+# Screenshot 
+![Screenshot of Password Generator](./assets/images/passwordgenerator.png) 
 
-This is a screenshot of my project. 
+# Acceptance Criteria
 
+    GIVEN I need a new, secure password
+	WHEN I click the button to generate a password
+	THEN I am presented with a series of prompts for password criteria
+	WHEN prompted for password criteria
+	THEN I select which criteria to include in the password
+	WHEN prompted for the length of the password
+	THEN I choose a length of at least 10 characters and no more than 130 characters
+	WHEN asked for character types to include in the password
+	THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
+	WHEN I answer each prompt
+	THEN my input should be validated and at least one character type should be selected
+	WHEN all prompts are answered
+	THEN a password is generated that matches the selected criteria
+	WHEN the password is generated
+	THEN the password is either displayed in an alert or written to the page
--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -8,19 +8,16 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
   // This selects all ids equal to password in the html file
   passwordText.value = password;
-
-}
+};
 
 // This function generates the password
 function generatePassword () {
     var passwordLength = prompt("How many characters will be in your password?");
     // This prompt will show on screen before the text is entered
-    var inLength = parseINT(passwordLength);
-    // parseINT returns an integer
-    console.log(inLength);
+    console.log(passwordLength);
 
     // Here we length characters for the text field
-    if (inLength < 10 || inLength > 130 || isNaN(inLength)) {
+    if (passwordLength < 10 || passwordLength > 130 || isNaN(passwordLength)) {
         alert ("Enter a number between 10 and 130.");
         return;
     }
@@ -39,15 +36,15 @@ if (lowerCaseLetters === true) {
   for (var i = 0; i < lowerCase.length; i++) {
     passwordCharacters.push(lowerCase[i]);
   }
-}
+};
 
 // upper case letters
 var upperCaseLetters = confirm("Would you like to add any upper case letters?");
 if (upperCaseLetters === true) {
   for (var i =0; i < upperCase.length; i++) {
-    passwordCharacters.push(upperCase[i]);
+    passwordCharacters.push(upperCase[i]); // .push adds one or more elements to the end of an array
   }
-}
+};
 
 // special characters
 var specialLetterCharacters = confirm("Would you like to add any special characters?");
@@ -55,7 +52,7 @@ if (specialLetterCharacters === true) {
   for (var i = 0; i < specialCharacters.length; i++) {
     passwordCharacters.push(specialCharacters[i]);
   }
-}
+};
 
 // numbers
 var numberCharacters = confirm("Would you like to add any numbers?");
@@ -63,13 +60,13 @@ var numberCharacters = confirm("Would you like to add any numbers?");
     for (var i = 0; i < numbers.length; i++) {
       passwordCharacters.push(numbers[i]);
     }
-  }
+  };
 
 // I followed the same if statement layout for each of the five variables on lines 30-34 
 
 // the last password the user enters
 var lastPassword = "";
-for (var i = 0; i < inLength; i++) {
+for (var i = 0; i < passwordLength; i++) {
     passwordCharacters[
       Math.floor(Math.random() * passwordCharacters.length)];
     lastPassword +=
