b24b03567f5541c923bbc705e3cbbdd8a4398f8d
Reggie P <reginaldpitts1@gmail.com>
2022-10-19 20:29:33 -0500

    Edited js code, added comments, started readme.md file

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # PasswordGenerator-3
+
+
+I created a password generator with JavaScript. The user is able to create a random password with special characters, numbers, upper case and lower case letters. The length has to be from 8 to 128 characters long. I was able to use several different variables, functions and for loops to create this. 
+
+live site: 
+
+This is a screenshot of my project. 
+
--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,84 @@
 // Assignment code here
-
-
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+const generateBtn = document.querySelector("#generate");
+// This selects all ids equal to generate in the html file
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
+  // This selects all ids equal to password in the html file
   passwordText.value = password;
 
 }
 
+// This function generates the password
+function generatePassword () {
+    var passwordLength = prompt("How many characters will be in your password?");
+    // This prompt will show on screen before the text is entered
+    var inLength = parseINT(passwordLength);
+    // parseINT returns an integer
+    console.log(inLength);
+
+    // Here we length characters for the text field
+    if (inLength < 10 || inLength > 130 || isNaN(inLength)) {
+        alert ("Enter a number between 10 and 130.");
+        return;
+    }
+
+
+// Variables for password characters
+var passwordCharacters = [];
+var lowerCase = "abcdefghiklmnopqrstuvwxyz" .split(""); 
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" .split("");
+var specialCharacters= "!@#$%^&*()-+?<>}{|~;" .split("");
+var numbers = "0123456789" .split (""); // we use the .split() method to separate the strings
+
+// lower case letters
+var lowerCaseLetters = confirm("Would you like to add any lower case letters?");
+if (lowerCaseLetters === true) {
+  for (var i = 0; i < lowerCase.length; i++) {
+    passwordCharacters.push(lowerCase[i]);
+  }
+}
+
+// upper case letters
+var upperCaseLetters = confirm("Would you like to add any upper case letters?");
+if (upperCaseLetters === true) {
+  for (var i =0; i < upperCase.length; i++) {
+    passwordCharacters.push(upperCase[i]);
+  }
+}
+
+// special characters
+var specialLetterCharacters = confirm("Would you like to add any special characters?");
+if (specialLetterCharacters === true) {
+  for (var i = 0; i < specialCharacters.length; i++) {
+    passwordCharacters.push(specialCharacters[i]);
+  }
+}
+
+// numbers
+var numberCharacters = confirm("Would you like to add any numbers?");
+  if(numberCharacters === true) {
+    for (var i = 0; i < numbers.length; i++) {
+      passwordCharacters.push(numbers[i]);
+    }
+  }
+
+// I followed the same if statement layout for each of the five variables on lines 30-34 
+
+// the last password the user enters
+var lastPassword = "";
+for (var i = 0; i < inLength; i++) {
+    passwordCharacters[
+      Math.floor(Math.random() * passwordCharacters.length)];
+    lastPassword +=
+      passwordCharacters[
+        Math.floor(Math.random() * passwordCharacters.length)];
+      }
+      return lastPassword;
+};
+
+
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword); 
